88a4c7fabd9bdfb2e0727d85a121f21581317eb0
axwii <axwi0001@stud.kea.dk>
2024-11-05 21:45:12 +0100

    Enhance Item component with improved styling and animation for better user interaction

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,21 @@
 "use client";
 import { useState } from "react";
+import { FiChevronDown } from "react-icons/fi";
 
 const Item = ({ title, children }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className="border-b border-gray-200">
-            <button onClick={() => setIsOpen(!isOpen)} className="w-full text-left py-2 px-4 bg-gray-100 hover:bg-gray-200 focus:outline-none text-black">{title}</button>
-            {isOpen && <div className="p-4 bg-white text-black">{children}</div>}
+            <button onClick={() => setIsOpen(!isOpen)} className={`flex items-center justify-between w-full text-left py-4 px-6 transition-colors duration-200 ${isOpen ? "bg-white text-gray-800" : "bg-[#F5F9FF] text-gray-700"} hover:bg-[#E0E8F8] focus:outline-none`}>
+                <span className="text-lg font-semibold">{title}</span>
+                <FiChevronDown  className={`w-5 h-5 transition-transform duration-200 ${isOpen ? "transform rotate-180" : ""}`} />
+            </button>
+            {isOpen && (
+                <div className="p-6 bg-white text-gray-800 shadow-md rounded-b-lg">
+                    {children}
+                </div>
+            )}
         </div>
     );
 };
